54cf44d367324e615cc6e5d0e2c19e11b972cb12
pylypivy <pylypivy14@my.sunyulster.edu>
2023-05-18 14:31:15 -0400

    RollABall game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+	
+	public float speed;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+
+	private float movementX;
+	private float movementY;
+
+	private Rigidbody rb;
+	private int count;
+
+	
+	void Start()
+	{
+		
+		rb = GetComponent<Rigidbody>();
+
+		
+		count = 0;
+
+		SetCountText();
+
+		
+		winTextObject.SetActive(false);
+	}
+
+	void FixedUpdate()
+	{
+		
+		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.gameObject.CompareTag("Pickup"))
+		{
+			other.gameObject.SetActive(false);
+
+			
+			count = count + 1;
+
+			
+			SetCountText();
+		}
+	}
+
+	void OnMove(InputValue value)
+	{
+		Vector2 v = value.Get<Vector2>();
+
+		movementX = v.x;
+		movementY = v.y;
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 12)
+		{
+			// Set the text value of your 'winText'
+			winTextObject.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+	// Before rendering each frame..
+	void Update()
+	{
+
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
